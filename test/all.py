d483f671ee973f9586eed997aaad284c73a8f432
Guillaume Papin <guillaume.papin@epitech.eu>
2016-07-06 01:25:03 +0200

    test: add all.py to run all tests

---
+++ test/all.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import os
+import unittest
+
+LOCAL_PATH = os.path.abspath(os.path.dirname(__file__))
+
+# alternatively: one can use the command line like this:
+#       python -m unittest discover '--pattern=*.py'
+if __name__ == '__main__':
+    testsuite = unittest.TestLoader().discover(LOCAL_PATH, pattern="*.py")
+    unittest.TextTestRunner(verbosity=1).run(testsuite)
