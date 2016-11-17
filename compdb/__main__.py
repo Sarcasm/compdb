0d6cdc4671fa69e2c7c8fdd4edd2d67e09b9a091
Guillaume Papin <guillaume.papin@epitech.eu>
2016-11-18 00:56:29 +0100

    make __main__.py work when directly invoked by python

--- compdb/__main__.py
+++ compdb/__main__.py
@@ -3,7 +3,11 @@ from __future__ import print_function, unicode_literals, absolute_import
 import os
 import sys
 
-import compdb
+# allow invokation of the style 'python /path/to/compdb'
+if __package__ == '':
+    sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+
+from compdb import main
 
 if __name__ == '__main__':
-    sys.exit(compdb.main())
+    sys.exit(main())
