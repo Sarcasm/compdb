9244286a1ad6dfe74a5019e78b46510129e8403c
Guillaume Papin <guillaume.papin@epitech.eu>
2017-02-17 12:43:20 +0100

    setup.py: update call to find_packages() to whitelist compdb, compdb.*
    The exclude argument with only 'tests' wasn't enough,
    tests subdirectories were being listed.
    
    I imagine a 'doc' or 'contrib' directory may be found as packages in the future,
    so use a whitelist instead to list only compdb and subpackages.

--- setup.py
+++ setup.py
@@ -43,9 +43,10 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     keywords=['Clang', 'compilation-database', 'compdb'],
-    packages=find_packages(exclude=['tests']),
+    packages=find_packages(include=['compdb', 'compdb.*']),
     test_suite="tests",
     entry_points={
         "console_scripts": [
